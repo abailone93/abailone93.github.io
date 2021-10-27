dd172390036dde0659165257ee3b4d39788af565
Alex <91600207+abailone93@users.noreply.github.com>
2021-10-27 11:29:10 +0200

    Update nomWebApp.js

---
+++ projecteEnBlanc/webEnBlanc/js/nomWebApp.js
@@ -0,0 +1,13 @@
+/*function ClasMoto(){
+    class Moto{
+        constructor(modelo,precio){
+            this.modelo=modelo;
+            this.precio=precio;
+        }
+    }
+    const myMoto = new Moto("Ducati",5000);
+    document.getElementById("m").innerHTML =
+    myMoto.name + " " + myMoto.year;
+}*/
+document.getElementById("prueba_inner").innerHTML = 11 + 6;
+document.write(11 + 6);
