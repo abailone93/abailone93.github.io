9c38365ec959b21a766cc5df5ef726b16deba65f
Alex <91600207+abailone93@users.noreply.github.com>
2021-10-27 11:38:55 +0200

    Update nomWebApp.js

--- projecteEnBlanc/webEnBlanc/js/nomWebApp.js
+++ projecteEnBlanc/webEnBlanc/js/nomWebApp.js
@@ -9,5 +9,8 @@
     document.getElementById("m").innerHTML =
     myMoto.name + " " + myMoto.year;
 }*/
-document.getElementById("prueba_inner").innerHTML = 11 + 6;
+window.alert("Hola, bienvenido gran y respetado cliente!");
+var numero = prompt("Introduce numero: ");
+confirm("pulsa el boton aceptar para continuar");
+document.getElementById("pruebainner").innerHTML = 11 + 6 + $numero;
 document.write(11 + 6);
