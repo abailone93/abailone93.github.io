ec4a67606a601ba627d27b57e8b36ee34137a3ac
abailone93 <91600207+abailone93@users.noreply.github.com>
2021-10-29 11:55:14 +0200

    update_29-10-2021

--- projecteEnBlanc/webEnBlanc/js/pruebas.js
+++ projecteEnBlanc/webEnBlanc/js/pruebas.js
@@ -5,3 +5,21 @@ confirm("pulsa el boton aceptar para continuar");
 const d = new Date();
 const mesos = ["Gener", "Febrer", "Març", "Abril", "Maig", "Juny", "Juliol", "Agost", "Setembre", "Octubre", "Novembre", "Decembre"];
 document.getElementById("demo").innerHTML = mesos[d.getMonth()];
+
+const day = new Date();
+document.getElementById("dia").innerHTML = day.getDate();
+
+const hour = new Date();
+document.getElementById("hora").innerHTML = hour.getHours();
+
+const mins = new Date();
+document.getElementById("minuts").innerHTML = mins.getMinutes();
+
+const secs = new Date();
+document.getElementById("segons").innerHTML = secs.getSeconds();
+
+const milsecs = new Date();
+document.getElementById("milisegons").innerHTML = milsecs.getMilliseconds();
+
+const weekday = new Date();
+document.getElementById("diasemana").innerHTML = weekday.getDay();
