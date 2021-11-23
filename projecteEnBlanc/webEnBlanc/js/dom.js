08cc46ed4311956e638c52e4f05c3261de41cee7
Alex <91600207+abailone93@users.noreply.github.com>
2021-11-23 13:20:32 +0100

    update_23-11-2021

--- projecteEnBlanc/webEnBlanc/js/dom.js
+++ projecteEnBlanc/webEnBlanc/js/dom.js
@@ -1,18 +1,22 @@
+/*DOM STYLE*/
 function domStyle(){
     document.body.style.backgroundColor = "#F0E0F2";
     document.getElementById("fila").style.border = "medium solid #E120F9";
     document.getElementById("sintaxis").style.font = "italic bold 2px";
 }
+/*DOM ELEMENT*/
 function domElement(){
-    document.getElementsByTagName("div")[1].setAttribute("style", "backgroung-color: #BD8FC3;");
+    var cajas = document.getElementsByTagName("div");
+    cajas.setAttribute("style", "backgroung-color: #BD8FC3;");
 }
+/*DOM DOCUMENT*/
 function domDocument(){
 
 }
+/*DOM EVENTS*/
 function domEvents(){
     document.getElementById("boton").addEventListener("mouseover",imagenload);
 }
 function imagenload(){
     document.getElementById("imagen").src = "../media/img/domStyle.PNG";
 }
-
