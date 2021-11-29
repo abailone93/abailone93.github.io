47e40578a25e435dfa2d0a7ee1beb3ff9983c15d
Alex <91600207+abailone93@users.noreply.github.com>
2021-11-29 13:18:06 +0100

    update_29-11-2021

--- projecteEnBlanc/webEnBlanc/UI/selectores.js
+++ projecteEnBlanc/webEnBlanc/UI/selectores.js
@@ -1,8 +1,14 @@
 function seleccionSelectores(){
     /*let select;
     select = document.getElementsByTagName('tr');*/
-    let selector = document.querySelectorAll('[sexo="mujer"]')
+    let selector = document.querySelectorAll('tr[sexo*="mujer"]')
     for (let i=0; i <= selector.length; i++){
-        selector[i].style.backgroundColor = "green";
+        selector[i].style.backgroundColor = "#75D860";
+    }
+
+    let selector2 = document.querySelectorAll('tr[sexo="hombre"]')
+    for (let i=0; i <= selector2.length; i++){
+        selector2[i].style.backgroundColor = "red";
+        selector2[i].style.color = "white";
     }
 }
