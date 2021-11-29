12cbb009ffe168b828a1124ff27814a6a990f623
Alex <91600207+abailone93@users.noreply.github.com>
2021-11-29 12:35:01 +0100

    update_29-11-2021

--- projecteEnBlanc/webEnBlanc/UI/selectores.js
+++ projecteEnBlanc/webEnBlanc/UI/selectores.js
@@ -1,9 +1,8 @@
 function seleccionSelectores(){
-    let select;
-    select = document.getElementsByTagName('tr');
-    let selector = document.querySelectorAll('sexo ^=m');
-    for (i=0; i <= selector.length; i++){
-        selector.backgroundColor = "green";
+    /*let select;
+    select = document.getElementsByTagName('tr');*/
+    let selector = document.querySelectorAll('[sexo="mujer"]')
+    for (let i=0; i <= selector.length; i++){
+        selector[i].style.backgroundColor = "green";
     }
-
 }
