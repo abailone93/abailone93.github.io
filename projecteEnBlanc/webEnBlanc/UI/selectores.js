a5a1225e539e1d3c32cb4c5e53c6141294f739c9
Alex <91600207+abailone93@users.noreply.github.com>
2021-12-16 12:22:39 +0100

    update_16-12-2021

--- projecteEnBlanc/webEnBlanc/UI/selectores.js
+++ projecteEnBlanc/webEnBlanc/UI/selectores.js
@@ -1,20 +1,23 @@
 function seleccionSelectores(){
-    /*let select;
-    select = document.getElementsByTagName('tr');*/
     let selector = document.querySelectorAll('tr[sexo*=mujer]')
     for (let i=0; i <= selector.length; i++){
         selector[i].style.backgroundColor = "#75D860";
     }
-    let selectorHombre = document.querySelectorAll('tr[sexo*=hombre]')
-    for (let i=0; i <= selectorHombre.length; i++){
-        selectorHombre[i].style.backgroundColor = "red";
-        selectorHombre[i].style.color = "white";
+}
+function cambiaHombres(){
+    let selectHombre = document.querySelectorAll('tr[sexo*=hombre]')
+    for (let m=0; m <= selectHombre.length; m++){
+        selectHombre[m].style.backgroundColor = "#FF0000";
+        selectHombre[m].style.color = "#FFFFFF";
     }
-    
+}   
+function orangePush(){
     let selector3 = document.querySelectorAll('*[sexo*="mujer"]')
     for (let i=0; i <= selector3.length; i++){
         selector3[i].style.backgroundColor = "orange";
     }
+}
+function ns(){
     //let selector4 = document.querySelectorAll('div[.resultado]')
     //selector4.push("19");
     //let selector5 = document.querySelectorAll('div[.resultado]')
