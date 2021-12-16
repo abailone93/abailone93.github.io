df1c9272cb3a69f3d34b513c6afe577a14796b30
Alex <91600207+abailone93@users.noreply.github.com>
2021-12-16 13:37:11 +0100

    update_16-12-2021

---
+++ projecteEnBlanc/webEnBlanc/Bootstrap_AlexBailon/index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>BootStrap Conceptes</title>
+        <link rel="stylesheet" href="../bootstrap-5.1.3-dist/bootstrap-5.1.3-dist/css/bootstrap-grid.min.css">
+        <link rel="stylesheet" href="../bootstrap-5.1.3-dist/bootstrap-5.1.3-dist/css/bootstrap.min.css">
+        <script src="../bootstrap-5.1.3-dist/bootstrap-5.1.3-dist/js/bootstrap.bundle.min.js"></script>
+        <script src="../bootstrap-5.1.3-dist/bootstrap-5.1.3-dist/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="./miCSS.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div id="col1" class="col-md-6 col-lg-4 bg-primary">
+                    <p class="text-white">Cel·la(1,1)</p>
+                </div>
+                <div id="col2" class="col-md-6 col-lg-8 bg-secondary">
+                    <p class="text-white">Cel·la(1,2)</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-4 bg-danger">
+                    <p class="text-white">Cel·la(2,1)</p>
+                </div>
+                <div class="col-4 bg-success">
+                    <p class="text-white">Cel·la(2,2)</p>
+                </div>
+                <div class="col-4 bg-danger">
+                    <p class="text-white">Cel·la(2,3)</p>
+                </div>
+            </div>
+            <div class="row">
+            <div class="col-sm-6 col-lg-3 bg-info">
+                    <p class="text-white">Cel·la(3,1)</p>
+                </div>
+                <div class="col-sm-6 col-lg-3 bg-dark">
+                    <p class="text-white">Cel·la(3,2)</p>
+                </div>
+                <div class="col-sm-6 col-lg-3 bg-info">
+                    <p class="text-white">Cel·la(3,3)</p>
+                </div>
+                <div class="col-sm-6 col-lg-3 bg-dark">
+                    <p class="text-white">Cel·la(3,4)</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
