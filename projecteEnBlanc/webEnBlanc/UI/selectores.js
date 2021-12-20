23f04235f365b9cd53cc9d294f609efc7cfebd63
Alex <91600207+abailone93@users.noreply.github.com>
2021-12-20 11:40:07 +0100

    update_20-12-2021

--- projecteEnBlanc/webEnBlanc/UI/selectores.js
+++ projecteEnBlanc/webEnBlanc/UI/selectores.js
@@ -18,16 +18,34 @@ function orangePush(){
     }
 }
 function edatBea(){
-    let selector4 = document.querySelectorAll('tr[sexo*=mujer] > td[19]');
-    jQuery('div.resultado').append(jQuery('tr[sexo*=mujer > td[19]'));
-    selector4.insertAdjacentText("afterend","19");
-    //jQuery('div.resultado').append('<td>19</td>');
-    //let selector5 = document.querySelectorAll('div[.resultado]')
-    //selector5.push("19,21");
-    //jQuery('tr[sexo*=mujer > td[19]').appendTo('.resultado');
-    //let selectorazo;
-/*
-https://lenguajejs.com/javascript/dom/crear-elementos-dom/ 
-https://lenguajejs.com/javascript/dom/insertar-elementos-dom/
-*/
+    //let selector4 = document.querySelectorAll('tr[sexo*=mujer] > td[19]');
+    //jQuery('div.resultado').append(jQuery('tr[sexo*=mujer > td[19]'));
+    /*
+    https://lenguajejs.com/javascript/dom/crear-elementos-dom/ 
+    https://lenguajejs.com/javascript/dom/insertar-elementos-dom/
+    */
+    let divResultado = document.getElementsByClassName("resultado");
+    divResultado[0].innerHTML = b[3].innerHTML + ", " + aitor[1].innerHTML;
+}
+function restarEdatDones(){
+    for (let i = 0; i < b.length; i++) {
+        if (parseInt(b[i].innerHTML)) {
+            let nuevaEdad = parseInt(b[i].innerHTML);
+            b[i].innerHTML = nuevaEdad - 1;
+        }
+    }
+}
+function menorsEdat(){
+    let total = document.querySelectorAll("tr>td");
+    let menoresEdad = [];
+    for (let i = 0; i < total.length; i++) {
+        let edat = total[i].innerHTML;
+        if (parseInt(edat)) {
+            if (parseInt(edat) < 18) {
+            let nombreMenor = total[i - 1].innerHTML;
+            menoresEdad.push(nombreMenor);
+            }
+        }
+    }
+    divResultado[0].innerHTML = "Llista de menors: " + menoresEdad;
 }
